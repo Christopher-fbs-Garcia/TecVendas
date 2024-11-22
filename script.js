52f0d90a5129b6856960fccc69e480d3a1ab2aef
Gabriel-Scapatici <gabriel.scapatici23@gmail.com>
2024-11-22 14:59:23 -0300

    ✨feat: EU FIZ, EU FIZ "TODO" O SITE

---
+++ script.js
@@ -0,0 +1,5 @@
+// Função para alternar a visibilidade do menu ao clicar no ícone de menu hambúrguer
+function toggleMenu() {
+    const navMenu = document.getElementById('navMenu');
+    navMenu.style.display = navMenu.style.display === 'flex' ? 'none' : 'flex';
+  }
